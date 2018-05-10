31c91f50694c7c9f4d1f9acfb3ab212898151d8a
Sydney Barlow <sydneybarlow44@gmail.com>
2018-05-10 13:40:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete destructivelyDeleteFromObjectByKey[key];
+  delete object[key];
 }
